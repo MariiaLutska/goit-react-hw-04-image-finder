9db3aeb9ab8bc55e85558b514c9e466b95dd848d
MariiaLutska <myklush@ukr.net>
2022-08-29 01:10:58 +0300

    Modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+import { Modal } from './Modal/Modal';
+// import { Box } from '../Box';
+
+
+export class App extends Component {
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+  render() {
+    const { todos, filter, showModal } = this.state;
+    return (
+      
+    
+      // <Box 
+      //   display="flex">
+        /* <Section>
+          <Searchbar />
+        </Section> */
+
+       /* <Section>
+          <ImageGallery />
+        </Section> */
+        <Modal>
+          <button type="button" onClick={this.toggleModal}>
+            Відкрити зображення
+          </button>
+
+          {showModal && (
+            <Modal onClose={this.toggleModal}>
+              <button type="button" onClick={this.toggleModal}>
+                Закрити
+              </button>
+            </Modal>)}
+        </Modal>
+        // </Box>
+    )
+  }
+}

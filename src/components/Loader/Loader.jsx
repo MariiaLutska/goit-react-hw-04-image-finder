6bb5850f295e4fe9409607a2fafcbd1539a2c1f0
MariiaLutska <myklush@ukr.net>
2022-09-07 16:22:42 +0300

    Loader style

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,18 @@
 import { Circles } from 'react-loader-spinner';
 
+const Container = {
+  position: 'fixed',
+  display: 'flex',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '30px',
+};
 export const Loader = () => {
   return (
-    <div display="flex"
-      alignitems="center"
-      justifycontent="center"
-      position="fixed"
-      top='50%'
-      left='50%'>
+    <div style={Container}>
       <Circles color='blue' height={120} width={120} />
     </div>
   );

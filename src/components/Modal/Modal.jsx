6a9ee565ddd33aeba338fc4cbfc636cc3e21f698
MariiaLutska <myklush@ukr.net>
2022-09-15 16:56:49 +0300

    hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,38 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import s from '../Modal/Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.closeModal();
-    }
+export const Modal = ({ closeModal, largeImage }) => {
+  useEffect(() => {
+    const onEscapeBtnClick = event => {
+      if (event.code === 'Escape')closeModal();
+    };
+
+    window.addEventListener('keydown', onEscapeBtnClick);
+
+    return () => {
+      window.removeEventListener('keydown', onEscapeBtnClick);
   };
-  handleBackdropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.closeModal();
-    }
-  };
-  render() {
-    return (
-      <div className={s.Overlay} onClick={this.handleBackdropClick}>
-        <div className={s.Modal}>
+}, [closeModal]);
+
+const onOverlayClick = event => {
+  if (event.target === event.currentTarget) {
+    closeModal();
+  }
+};
+
+return (
+  <div className={s.Overlay} onClick={onOverlayClick}>
+    <div className={s.Modal}>
           <img
             className={s.Image}
-            src={this.props.largeImage}
+            src={largeImage}
             alt="choosed foto"
           />
-        </div>
-      </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
+};
 
 Modal.propTypes = {
   closeModal: PropTypes.func.isRequired,
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 // import Notiflix from 'notiflix';
 import { ImSearch } from 'react-icons/im';
 import s from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-    state = {
-        value: '',
-    };
+export const Searchbar = ({onSubmit}) => {
+  const [value, setValue] = useState('');
 
-    handleChange = e => {
-     this.setState({ value: e.currentTarget.value.toLowerCase() });
-  };
-
-    handleSubmit = e => {
+    const handleSubmit = e => {
       e.preventDefault();
-      const searchValue =this.state.value.trim()
-        if (searchValue === '') {
+      
+      const searchValue = value.trim()
+
+        if (!searchValue) {
         alert('Введіть імя картинки.')
             return;
         }
-        this.props.onSubmit(searchValue);
-        this.setState({ value: '' });
+        onSubmit(searchValue);
+        setValue('');
     };
 
-    render() {
+ 
       return (
        <header className={s.Searchbar}>
-        <form onSubmit={this.handleSubmit} className={s.SearchForm}>
+        <form onSubmit={handleSubmit} className={s.SearchForm}>
             <button type="submit" className={s.Button}>
               <ImSearch style={{ marginRight: 10 }} />
               {/* <span>Search</span> */}
@@ -38,14 +34,13 @@ export class Searchbar extends Component {
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
-            onChange={this.handleChange}
-            value={this.state.value}
+            onChange={e=>setValue(e.target.value)}
+            value={value}
           />
         </form>
       </header>
     );
   }
-}
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
